5bdedcaa0194237df17e04ed691f9bbf6ddd3d57
Rahul Rai <rahulrairr2014@gmail.com>
2022-11-20 20:15:40 +0530

    introducing states

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
-  
+  state = {
+    count: 0,
+  };
+
+  formatCount() {
+    // without using object destructuring
+    // return this.state.count ? "Zero" : this.state.count;
+
+    // using object destructuring
+    var { count } = this.state;
+    // return count ? count : "Zero"; // my expression
+
+    return count === 0 ? "Zero" : count;
+  }
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
